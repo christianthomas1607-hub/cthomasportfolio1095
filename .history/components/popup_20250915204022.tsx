5fe335873944500f2c0809a09c84429490727771
cthoma61 <98545222+cthoma61@users.noreply.github.com>
2025-09-15 20:43:52 -0400

    Button color and position change.

--- .history/components/popup_20250915204022.tsx
+++ .history/components/popup_20250915204022.tsx
@@ -4,7 +4,7 @@ export default function Popup({ onClose, item }: { onClose: () => void, item: { 
     <button
         id="popUpCloseButton"
           onClick={onClose}
-          className="rounded-md bg-blue-800 px-3.5 py-2.5 text-4xl font-semibold text-white shadow-sm hover:bg-bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 shadow-xl/30 fixed top-4 right-4 z-50"
+          className="rounded-md bg-blue-800 px-3.5 py-2.5 text-4xl font-semibold text-white shadow-sm hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 shadow-lg"
         >
      <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="4" stroke-linecap="butt" stroke-linejoin="arcs"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
 Close
@@ -19,7 +19,7 @@ Close
     {/* Inner content */}
     <div className="">
       <div className="">
-        <p className="text-base font-semibold text-blue-600">Deploy faster</p>
+        <p className="text-base font-semibold text-indigo-600">Deploy faster</p>
         <h1 className="mt-2 text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
           {item ? item.title : 'No item selected'}
         </h1>
