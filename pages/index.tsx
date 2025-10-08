4f536d2e29a7cfdc0eb8ddefa219f44c755678e3
cthomas@cookcompanies.net <cthomas@cookcompanies.net>
2025-10-08 08:11:16 -0400

    Controls page addition.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import { useRef, useState } from 'react'
 import TexturedBox from '../components/TexturedBox'
 import Popup from '../components/popup'
 import { WordAndImage as WordAndImageType } from '../components/data'
+import Controls from '../components/Controls'
 
 // function simulateKeyEvent(key: string, type: 'keydown' | 'keyup') {
 //   window.dispatchEvent(new KeyboardEvent(type, { key }))
@@ -14,26 +15,8 @@ import { WordAndImage as WordAndImageType } from '../components/data'
 
 
 
-function simulateKeyEvent( key: string, type: 'keydown' | 'keyup') {
-  window.dispatchEvent(new KeyboardEvent(type, {key}))
-}
 
 
-
-function PreventFall(b : { controllerRef: React.RefObject<any> }) {
-
-
-useFrame(() => {
-  const c = b.controllerRef.current
-  if (!c) return
-  const minY = 0    // world floor height
-  const t = c.translation() // or c.rigidBody.translation()
-  if (t.y < minY) {
-   return c.setTranslation({ x: t.x, y: minY, z: t.z }, true)
-  }
-})
-
-}
 
 
 export default function Page() {
@@ -68,46 +51,7 @@ const [selectedItem, setSelectedItem] = useState<WordAndImageType | null>(null)
   return (
     <div style={{ width: '100vw', height: '100vh', overflow: 'hidden', margin: 0 }}>
       {showPopup ? <Popup onClose={handleClosePopup} item={selectedItem}  /> : null}
-      <div className="controls">
-        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-black mx-2 py-2 px-4 rounded inline-flex items-center"
-          onPointerDown={() => { simulateKeyEvent('w', 'keydown'); simulateKeyEvent('ArrowUp', 'keydown') }}
-          onPointerUp={() => { simulateKeyEvent('w', 'keyup'); simulateKeyEvent('ArrowUp', 'keyup') }}
-          onPointerLeave={() => { simulateKeyEvent('w', 'keyup'); simulateKeyEvent('ArrowUp', 'keyup') }}
-        >
-          <svg className="mr-1" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="3.25" stroke-linecap="butt" stroke-linejoin="arcs"><path d="M12 19V6M5 12l7-7 7 7"/>
-          </svg>
-          forward
-        </button>
-        <div className="controls-middle-row">
-        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-black mx-2 py-2 px-4 w-24 rounded inline-flex items-center text-center"
-          onPointerDown={() => { simulateKeyEvent('a', 'keydown'); simulateKeyEvent('ArrowLeft', 'keydown') }}
-          onPointerUp={() => { simulateKeyEvent('a', 'keyup'); simulateKeyEvent('ArrowLeft', 'keyup') }}
-          onPointerLeave={() => { simulateKeyEvent('a', 'keyup'); simulateKeyEvent('ArrowLeft', 'keyup') }}
-        >
-           <svg className="mr-1" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="3.25" stroke-linecap="butt" stroke-linejoin="arcs"><path d="M19 12H6M12 5l-7 7 7 7"/>
-           </svg>
-          left
-        </button>
-        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-black mx-2 py-2 px-4 w-24 rounded inline-flex items-center text-center"
-          onPointerDown={() => { simulateKeyEvent('d', 'keydown'); simulateKeyEvent('ArrowRight', 'keydown') }}
-          onPointerUp={() => { simulateKeyEvent('d', 'keyup'); simulateKeyEvent('ArrowRight', 'keyup') }}
-          onPointerLeave={() => { simulateKeyEvent('d', 'keyup'); simulateKeyEvent('ArrowRight', 'keyup') }}
-        >
-          right
-          <svg className="ml-1" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="3.25" stroke-linecap="butt" stroke-linejoin="arcs"><path d="M5 12h13M12 5l7 7-7 7"/>
-          </svg>
-        </button>
-        </div>
-        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-black mx-2 py-2 px-4 rounded inline-flex items-center"
-          onPointerDown={() => { simulateKeyEvent('s', 'keydown'); simulateKeyEvent('ArrowDown', 'keydown') }}
-          onPointerUp={() => { simulateKeyEvent('s', 'keyup'); simulateKeyEvent('ArrowDown', 'keyup') }}
-          onPointerLeave={() => { simulateKeyEvent('s', 'keyup'); simulateKeyEvent('ArrowDown', 'keyup') }}
-        >
-           <svg className="mr-1" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="3.25" stroke-linecap="butt" stroke-linejoin="arcs"><path d="M12 5v13M5 12l7 7 7-7"/>
-           </svg>
-          backward
-        </button>
-      </div>
+      <Controls />
       <Canvas>
         <Environment 
         files="/images/ice_planet_close.jpg" 
@@ -136,7 +80,7 @@ const [selectedItem, setSelectedItem] = useState<WordAndImageType | null>(null)
             
 
             >
-              <PreventFall controllerRef={controllerRef} />
+              
               {/* <PreventFall /> */}
               {/* <Gltf castShadow receiveShadow scale={.1} position={[0, -.75, 0]} src="/images/probe-transformed.glb" /> */}
               <Gltf castShadow receiveShadow scale={.005} position={[0, -.75, 0]} src="/images/r2d2.glb" />
