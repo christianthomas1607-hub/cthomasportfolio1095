b7f9ddabc2b9cd76e9d9328ae56f1605245df0f8
cthomas@cookcompanies.net <cthomas@cookcompanies.net>
2026-03-09 11:21:30 -0400

    Loading page.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,35 +3,53 @@ import Layout from '../components/layout';
 import Router from 'next/router'
 import { useState, useEffect } from 'react';
 import Loader from '../components/loader';
-
+import Loading from '../components/loading';
+import { Suspense } from 'react'
 
 
 
 export default function App({ Component, pageProps }) {
 
-const [isLoading, setIsLoading] = useState(false);
+// const [isLoading, setIsLoading] = useState(false);
 
-useEffect(() => {
-  Router.events.on("routeChangeStart", (url) => {
-      setIsLoading(true)
-    });
+// useEffect(() => {
+//   Router.events.on("routeChangeStart", () => {
+//       setIsLoading(true)
+//     });
 
-    Router.events.on("routeChangeComplete", (url)=>{
-      setIsLoading(false)
-    });
+//     Router.events.on("routeChangeComplete", ()=>{
+//       setIsLoading(false)
+//     });
 
-    Router.events.on("routeChangeError", (url) =>{
-      setIsLoading(false)
-    });
+//     Router.events.on("routeChangeError", () =>{
+//       setIsLoading(false)
+//     });
 
-}, [Router]);
+//     return () => {
+//       Router.events.off("routeChangeStart", () => {
+//       setIsLoading(true)
+//     });
+
+//     Router.events.off("routeChangeComplete", ()=>{
+//       setIsLoading(false)
+//     });
+
+//     Router.events.off("routeChangeError", () =>{
+//       setIsLoading(false)
+//     });
+
+//     }
+
+// }, []);
 
 
   return (
     
     <Layout>
-      {isLoading && <Loader/>}
+      {/* {isLoading && <Loader/>} */}
+      <Suspense fallback={<Loading/>}>
       <Component {...pageProps} />
+      </Suspense>
     </Layout>
   
   );
